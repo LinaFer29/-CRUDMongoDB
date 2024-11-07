20c401d373dd9fbce1f214ad076dd387a8ea2cd1
Lina <linamfer0420@gmail.com>
2024-11-07 10:03:46 -0500

    Agregue un proyecto de práctica de MongoDB completado: CRUD con MongoTemplate y repositorio personalizado

---
+++ mdb-sprint-boot/src/main/java/com/example/fernandez/repository/CustomItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.fernandez.repository;
+
+public interface CustomItemRepository {
+	
+	void updateItemQuantity(String itemName, float newQuantity);
+
+}
---
+++ mdb-sprint-boot/src/test/java/com/example/mdb_sprint_boot/MdbSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdb_sprint_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdbSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
